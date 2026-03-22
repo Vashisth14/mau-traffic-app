eba69036bbf49c267199ffcab213b2457c83e4f5
Vashisth14 <vashisthittoo@gmail.com>
2026-03-23 01:39:05 +0400

    feat: update dependencies and remove unused import in report accident page

--- lib/features/accident_report/presentation/pages/report_accident_page.dart
+++ lib/features/accident_report/presentation/pages/report_accident_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../../../core/constants/app_strings.dart';
 
 class ReportAccidentPage extends StatefulWidget {
   const ReportAccidentPage({super.key});
